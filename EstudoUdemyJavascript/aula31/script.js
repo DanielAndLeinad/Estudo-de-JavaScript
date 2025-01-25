cea03f5998a34f8eca6ebfc47fb8bcfd07a9ff1a
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-01-24 21:26:14 -0300

    Create script.js

---
+++ EstudoUdemyJavascript/aula31/script.js
@@ -0,0 +1,18 @@
+/*
+function soma(x, y) {
+        const resultado = x + y;
+return resultado;
+}
+
+console.log(soma(2, 2));
+
+*/
+
+const raiz = function(n){
+return n ** 0.5
+};
+
+console.log(raiz(16));
+console.log(raiz(52));
+console.log(raiz(50));
+
