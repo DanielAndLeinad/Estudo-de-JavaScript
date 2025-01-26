b196a69c8cdb752153b29ea9d0bf5ea8cd68bafb
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-01-26 11:40:35 -0300

    Create script.js

---
+++ EstudoUdemyJavascript/aula31b/script.js
@@ -0,0 +1,33 @@
+
+/*
+function saudacao(nome) {
+   return `bom dia, ${nome}`
+    
+}
+
+const variavel = saudacao('marcos');
+console.log(variavel);
+
+//----------------------------------------------------------------
+function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma();
+console.log(resultado);
+
+
+//console.log(soma(2,2));
+//console.log(soma(5,3));
+//console.log(soma(5,6));
+
+//----------------------------------------------------------------
+*/
+
+const raiz = (n) => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
