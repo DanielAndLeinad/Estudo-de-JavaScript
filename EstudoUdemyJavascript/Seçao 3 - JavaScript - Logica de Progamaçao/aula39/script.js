0783ee74018865db2460e5c415dc031b9f51b612
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-01-27 00:06:16 -0300

    Create script.js

---
+++ EstudoUdemyJavascript/Seçao 3 - JavaScript - Logica de Progamaçao/aula39/script.js
@@ -0,0 +1,23 @@
+/*
+OPERADORES LOGICOS
+&& -> AND -> E 
+|| -> OR -> OU  
+! -> NOT -> NAO
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+
+const a = 0;
+const b = null;
+const c = 'false'; // STRING, LOGO, VERDADEIRO;
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e);
+
+
