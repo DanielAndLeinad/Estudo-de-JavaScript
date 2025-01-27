123f351936c105c868f93f41d5ee89a4b5f52ad7
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-01-26 21:59:16 -0300

    !!

---
+++ EstudoUdemyJavascript/Seçao 3 - JavaScript - Logica de Progamaçao/aula37/script.js
@@ -0,0 +1,44 @@
+/*
+operadores de comparaçao
+
+> - maior que
+
+>= maior que ou igual a que
+
+< - menor que
+
+<= - menor que ou igual a que
+
+==  - igualdade (VALOR) ********************************
+
+==== - igualdade estrita (VALOR E TIPO)
+
+!= - diferente (VALOR) ********************************
+
+!== - diferente estrita (VALOR E TIPO)
+
+
+*/
+const maiorQ = 10 > 5
+console.log (maiorQ);
+
+const maiorQou = 10 >= 11
+console.log (maiorQou);
+
+const menorQ = 5 < 10
+console.log (menorQ);
+
+const menorQou = 10 <= 5
+console.log (menorQou);
+
+const igual = 10 == 10
+console.log (igual);
+
+const igualEstrito = 10 === 11
+console.log (igualEstrito);
+
+const diferente = 10 != 10
+console.log (diferente);
+
+const diferenteEstrito = 10 !== 10
+console.log (diferenteEstrito);
---
+++ EstudoUdemyJavascript/Seçao 3 - JavaScript - Logica de Progamaçao/aula38/script.js
@@ -0,0 +1,16 @@
+/*
+OPERADORES LOGICOS
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NAO
+
+*/
+
+// EXEMPLOS LOGICOS
+
+
+
+console.log(true && true); // os valores precisam ser verdadeiro para retornar true
+console.log(true || false); // um é verdadeiro, então retorna tru
+console.log(!true); // inverte uma expressao. Se for true = false & se for false = true
+console.log(!!false); // invertendo o valor duas vezes. Uma vez para true e depois para false (!!) 
