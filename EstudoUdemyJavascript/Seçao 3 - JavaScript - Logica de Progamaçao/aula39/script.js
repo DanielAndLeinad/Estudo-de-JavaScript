044c4a1c2743956eec714f5258a509e2882d2945
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-01-27 19:17:24 -0300

    Update script.js

--- EstudoUdemyJavascript/Seçao 3 - JavaScript - Logica de Progamaçao/aula39/script.js
+++ EstudoUdemyJavascript/Seçao 3 - JavaScript - Logica de Progamaçao/aula39/script.js
@@ -16,7 +16,7 @@ const a = 0;
 const b = null;
 const c = 'false'; // STRING, LOGO, VERDADEIRO;
 const d = false;
-const e = NaN;
+const e = NaN; // RETORNARIA ISSO CASO FOSSE TUDO FALSO, POIS PEGA O ULTIMO FALSO A SER;;
 
 console.log(a || b || c || d || e);
 
