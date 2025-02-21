7331e5875182596197428b233e9fd305e45b2545
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-02-21 00:25:25 -0300

    !

--- EstudoUdemyJavascript/Seçao 3 - JavaScript - Logica de Progamaçao/aula49/index.js
+++ EstudoUdemyJavascript/Seçao 3 - JavaScript - Logica de Progamaçao/aula49/index.js
@@ -1,7 +1,39 @@
-let nome = 'luiz';
-var nome2 = 'luiz';
-const nome3 = 'luiz';
 
-if(verdadeira){
-    console.log('ok');
+/*
+const vdd = true;
+
+let nome = "luiz"; // criando
+var nome2 = "luiz"; // criando
+
+if (vdd) {
+  // escopo de bloco { ... bloco de código ... }
+  let nome = "otavio"; // criando
+  var nome2 = 'Rogerio'; // redeclarando
+  // console.log(nome, nome2);
+
+  if (vdd) {
+    // escopo de bloco { ... bloco de código ... } // criando
+    let nome = "otra cosa"; // criando
+    var nome2 = "otavio"; // redeclarando
+    console.log(nome, nome2);
+
+    // console.log(nome, nome2);
+  }
 }
+*/
+
+// var sobrenome = 'Miranda';
+
+// function falaOi(){
+//     var nome = 'Luiz';
+//     if (true){
+//         console.log(sobrenome);
+//     }
+// }
+
+// falaOi();
+
+
+console.log(sobrenome);
+
+var sobrenome = 'Miranda';
